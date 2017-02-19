5fdd8d96cb3b2e133ff8303adcc3d387f16797ca
Riliumph <riliumph@gmail.com>
2017-02-19 18:34:39 +0900

    [Add] Solution of ShFileOperation
    include ShFileOperator

---
+++ ShFileOperation/ShFileOperation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace ShFileOperation
+{
+	using Extender;
+	using WinApiUtil;
+	class Program
+	{
+		static void Main( string [] args )
+		{
+			var copy_list = new List<string> { @"c:\aaa.txt", @"c:\bbb.txt", };
+
+			var copy_operation = new SHFileOperator( SHFileOperator.Operations.COPY );
+			copy_operation.pFrom = ",".Join( copy_list );
+
+
+		}
+	}
+}
--- ShFileOperation/ShFileOperation/SHFileOperator.cs
+++ ShFileOperation/ShFileOperation/SHFileOperator.cs
@@ -12,7 +12,7 @@ namespace WinApiUtil
 		internal partial class NativeMethod
 		{
 			[DllImport( "shell32.dll", CharSet = CharSet.Unicode )]
-			public static extern int SHFileOperation( [In] ref Info lpFileOp );
+			public static extern int SHFileOperation( [In] ref OperationInfo lpFileOp );
 		}
 
 		public enum Operations : uint
@@ -26,7 +26,7 @@ namespace WinApiUtil
 		/// <summary>
 		/// 実行時にAPIへ渡す構造体の定義
 		/// </summary>
-		public struct Info
+		public struct OperationInfo
 		{
 			public IntPtr hwnd;
 			public Operations wFunc;
@@ -42,20 +42,90 @@ namespace WinApiUtil
 		}
 		#endregion
 
+		#region Fields
 		/// <summary>
-		/// コンストラクタ
+		/// SHFileOperation関数へ渡される構造体のインスタンス
+		/// ushortのビットフラグを公開したくないので、インスタンスはprivateにして各種アクセサを用意します。
+		/// </summary>
+		private OperationInfo _ope_info = new OperationInfo();
+
+		/// <summary>
+		/// SHFileOperationの詳細設定フラグをまとめたクラスインスタンス
+		/// 中身はすべて公開されているので、そのまま返す
+		/// </summary>
+		public OperationDetail _ope_detail { get; } = new OperationDetail();
+
+		/// <summary>
+		/// ウィンドウハンドル
+		/// </summary>
+		public IntPtr WindowHandle { set { _ope_info.hwnd = value; } }
+
+		/// <summary>
+		/// 処理モード
+		/// </summary>
+		public Operations Mode { set { _ope_info.wFunc = value; } }
+
+		/// <summary>
+		/// 処理From
+		/// 末尾の"\0\0"を自動付与する。
+		/// フルパスでないファイル名が指定された場合、カレントフォルダと見做されます。
+		/// </summary>
+		public string pFrom { set { _ope_info.pFrom = value + '\0' + '\0'; } }
+
+		/// <summary>
+		/// 処理To
+		/// 末尾の"\0\0"を自動付与する。
+		/// </summary>
+		public string pTo { set { _ope_info.pTo = value + '\0' + '\0'; } }
+
+		/// <summary>
+		/// 指定したファイル操作が完了する前にユーザーによって中止されたことを示します。
+		/// </summary>
+		public bool IsAborted { get { return _ope_info.fAnyOperationsAborted; } }
+
+		/// <summary>
+		/// 移動・コピー・名前変更されたファイルの古いファイル名と新しいファイル名を含むファイル名マッピングオブジェクトのハンドルを返します。
+		/// このメンバは fFlags メンバに FOF_WANTMAPPINGHANDLE フラグが指定された場合にのみ使用されます。
+		/// このハンドルが不要になったら SHFreeNameMappings 関数で解放しなくてはなりません。
+		/// </summary>
+		public IntPtr NameMappingsHandle { get { return _ope_info.hNameMappings; } }
+
+		/// <summary>
+		/// タイトルバーの表示する文言。
+		/// 末尾の"\0"を自動付与する。
+		/// </summary>
+		public string TitleBarName { set { _ope_info.lpszProgressTitle = value + '\0'; } }
+		#endregion
+
+		/// <summary>
+		/// デフォルト・コンストラクタ
 		/// </summary>
 		public SHFileOperator()
 		{
-			_ShFile.hwnd = IntPtr.Zero;
-			_ShFile.wFunc = Operations.COPY;
-			_ShFile.pFrom = "";
-			_ShFile.pTo = "";
-			_ShFile.fAnyOperationsAborted = false;
-			_ShFile.hNameMappings = IntPtr.Zero;
-			_ShFile.lpszProgressTitle = "";
+			_ope_info.hwnd = IntPtr.Zero;
+			_ope_info.wFunc = Operations.COPY;
+			_ope_info.pFrom = "";
+			_ope_info.pTo = "";
+			_ope_info.fAnyOperationsAborted = false;
+			_ope_info.hNameMappings = IntPtr.Zero;
+			_ope_info.lpszProgressTitle = "";
 		}
 
+		/// <summary>
+		/// コンストラクタ
+		/// 先行して、処理モードのみを設定できます。
+		/// </summary>
+		/// <param name="mode">操作内容</param>
+		public SHFileOperator( Operations mode )
+		{
+			_ope_info.hwnd = IntPtr.Zero;
+			_ope_info.wFunc = mode;
+			_ope_info.pFrom = "";
+			_ope_info.pTo = "";
+			_ope_info.fAnyOperationsAborted = false;
+			_ope_info.hNameMappings = IntPtr.Zero;
+			_ope_info.lpszProgressTitle = "";
+		}
 
 		/// <summary>
 		/// 実行関数
@@ -63,70 +133,14 @@ namespace WinApiUtil
 		/// <returns></returns>
 		public int Execute()
 		{
-			_ShFile.fFlags = Config.FlagsToBit();
-			return NativeMethod.SHFileOperation( ref _ShFile );
+			_ope_info.fFlags = _ope_detail.FlagsToBit();
+			return NativeMethod.SHFileOperation( ref _ope_info );
 		}
 
-		#region Fields
 		/// <summary>
-		/// SHFileOperation関数へ渡される構造体のインスタンス
-		/// ushortのビットフラグを公開したくないので、インスタンスはprivateにして各種アクセサを用意します。
+		/// シェルオペレーションの詳細設定フラグ群
 		/// </summary>
-		private Info _ShFile = new Info();
-
-		/// <summary>
-		/// SHFileOperationの詳細設定フラグをまとめたクラスインスタンス
-		/// 中身はすべて公開されているので、
-		/// </summary>
-		public Detail Config { get; } = new Detail();
-
-		/// <summary>
-		/// ウィンドウハンドル
-		/// </summary>
-		public IntPtr WindowHandle { set { _ShFile.hwnd = value; } }
-
-		/// <summary>
-		/// 処理モード
-		/// </summary>
-		public Operations Mode { set { _ShFile.wFunc = value; } }
-
-		/// <summary>
-		/// 処理From
-		/// 末尾の"\0\0"を自動付与する。
-		/// フルパスでないファイル名が指定された場合、カレントフォルダと見做されます。
-		/// </summary>
-		public string pFrom { set { _ShFile.pFrom = value + '\0' + '\0'; } }
-
-		/// <summary>
-		/// 処理To
-		/// 末尾の"\0\0"を自動付与する。
-		/// </summary>
-		public string pTo { set { _ShFile.pTo = value + '\0' + '\0'; } }
-
-		/// <summary>
-		/// 指定したファイル操作が完了する前にユーザーによって中止されたことを示します。
-		/// </summary>
-		public bool IsAborted { get { return _ShFile.fAnyOperationsAborted; } }
-
-		/// <summary>
-		/// 移動・コピー・名前変更されたファイルの古いファイル名と新しいファイル名を含むファイル名マッピングオブジェクトのハンドルが格納されます。
-		/// このメンバは fFlags メンバに FOF_WANTMAPPINGHANDLE フラグが指定された場合にのみ使用されます。
-		/// このハンドルが不要になったら SHFreeNameMappings 関数で解放しなくてはなりません。
-		/// </summary>
-		public IntPtr NameMappingsHandle { get { return _ShFile.hNameMappings; } }
-
-		/// <summary>
-		/// タイトルバーの表示する文言。
-		/// 末尾の"\0"を自動付与する。
-		/// </summary>
-		public string TitleBarName { set { _ShFile.lpszProgressTitle = value + '\0'; } }
-		#endregion
-
-
-		/// <summary>
-		/// 
-		/// </summary>
-		public class Detail
+		public class OperationDetail
 		{
 			private enum ConfigFlag : ushort
 			{
